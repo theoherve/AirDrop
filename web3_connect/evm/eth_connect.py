7f414b46bcc7ea48fb8761af44f1ed276802e5db
Mawaren <marwane.belaid@etu.univ-paris1.fr>
2023-06-03 17:23:15 +0200

    Starting_project

--- web3_connect/evm/eth_connect.py
+++ web3_connect/evm/eth_connect.py
@@ -3,7 +3,8 @@ from web3 import Web3, EthereumTesterProvider
 w3 = Web3(Web3.EthereumTesterProvider())
 account = w3.eth.accounts[0]
 
-print(w3.eth.get_balance(account, 'ether'))
+print(w3.eth)
+
 
 
 
